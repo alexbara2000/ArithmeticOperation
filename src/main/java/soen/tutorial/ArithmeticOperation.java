85f5c08aed2434c300ef9055a267146284d5c958
alexbara2000 <60298505+alexbara2000@users.noreply.github.com>
2022-03-28 22:09:57 -0400

    Update ArithmeticOperation.java

--- src/main/java/soen/tutorial/ArithmeticOperation.java
+++ src/main/java/soen/tutorial/ArithmeticOperation.java
@@ -5,9 +5,9 @@ public class ArithmeticOperation {
 	public Integer addOrSub(Integer a, Integer b)
 	{
 		if (a > b) {
-			return a - b;
-		} else {
 			return a + b;
+		} else {
+			return a - b;
 		}
 	}
 }
